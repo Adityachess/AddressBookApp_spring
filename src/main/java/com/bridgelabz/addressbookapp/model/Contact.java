4f6ae20f47ee0086257a8bf6a086654d50e7bde9
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-24 19:47:22 +0530

    Added:Intyroducing DTO and Model layer in the address book app.

--- src/main/java/com/bridgelabz/addressbookapp/model/Contact.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/Contact.java
@@ -1,22 +1,89 @@
 package com.bridgelabz.addressbookapp.model;
 
+import com.bridgelabz.addressbookapp.dto.ContactDTO;
+
 public class Contact {
-	private String id;
-	private String name;
+	private int contactId;
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String state;
+    public String city;
+    public String zip;
+    public String phone;
 
-	public String getId() {
-		return id;
-	}
+    public Contact(int contactId, ContactDTO contactDTO) {
+        this.contactId = contactId;
+        this.firstName = contactDTO.firstName;
+        this.lastName = contactDTO.lastName;
+        this.address = contactDTO.address;
+        this.state = contactDTO.state;
+        this.city = contactDTO.city;
+        this.zip = contactDTO.zip;
+        this.phone = contactDTO.phone;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public int getContactId() {
+        return contactId;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
